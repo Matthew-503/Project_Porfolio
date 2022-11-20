a40682c57c4396ef7403e6a357b02b00285ff0d1
Matthew Ng <nglingshan30@gmail.com>
2022-11-20 15:27:23 -0700

    Add cards in project section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,13 +1,72 @@
 import React from 'react';
 
-import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
-import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
+import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles'; // Styling
+import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents'; // Styling
+
+// project properties will be taked from here
 import { projects } from '../../constants/constants';
 
 const Projects = () => (
-  <div>
-    Projects
-  </div>
+  <Section nopadding id="projects">
+
+    {/* Section Divider Line */}
+    <SectionDivider />
+
+    {/* Project Header */}
+    <SectionTitle main>
+      Projects
+    </SectionTitle>
+
+    <GridContainer>
+      {
+        // all project properties used will be extracted from constant.js
+        // all all project properties used will be listed in the map area to avoid repeation 
+        projects.map(({ id, image, title, description, tags, source, visit }) => (
+          <BlogCard key={id}>
+            <Img src={image} />
+
+            <TitleContent>
+              {/* HeaderThree contains the title property */}
+              <HeaderThree title>
+                {title}
+              </HeaderThree>
+
+              {/* Dividing Line */}
+              <Hr />
+            </TitleContent>
+
+            <CardInfo>
+              {description}
+            </CardInfo>
+
+            <div>
+              <TitleContent>
+                Stack
+              </TitleContent>
+
+              <TagList>
+                {tags.map((tag, i) => (
+                  <Tag key={i}>
+                    {tag}
+                  </Tag>
+                ))}
+              </TagList>
+            </div>
+
+            <UtilityList>
+              <ExternalLinks href={visit}>
+                Code
+              </ExternalLinks>
+
+              <ExternalLinks href={source}>
+                Source
+              </ExternalLinks>
+            </UtilityList>
+          </BlogCard>
+        ))
+      }
+    </GridContainer>
+  </Section>
 );
 
 export default Projects;
