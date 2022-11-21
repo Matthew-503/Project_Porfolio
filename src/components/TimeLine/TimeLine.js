2335d0958a128309bb8bbbc3da668726889d2d27
Matthew Ng <nglingshan30@gmail.com>
2022-11-20 21:22:36 -0700

    Completed Timeline

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -10,74 +10,124 @@ const Timeline = () => {
   const [activeItem, setActiveItem] = useState(0);
   const carouselRef = useRef();
 
-  // const scroll = (node, left) => {
-  //   return node.scrollTo({ left, behavior: 'smooth' });
-  // }
+  const scroll = (node, left) => {
+    return node.scrollTo({ left, behavior: 'smooth' });
+  }
 
   const handleClick = (e, i) => {
     e.preventDefault();
 
     if (carouselRef.current) {
       const scrollLeft = Math.floor(carouselRef.current.scrollWidth * 0.7 * (i / TimeLineData.length));
+      
+      scroll(carouselRef.current, scrollLeft);
+    }
+  }
 
-      //     scroll(carouselRef.current, scrollLeft);
-      //   }
-      // }
+  const handleScroll = () => {
+    if (carouselRef.current) {
+      const index = Math.round((carouselRef.current.scrollLeft / (carouselRef.current.scrollWidth * 0.7)) * TimeLineData.length);
 
-      // const handleScroll = () => {
-      //   if (carouselRef.current) {
-      //     const index = Math.round((carouselRef.current.scrollLeft / (carouselRef.current.scrollWidth * 0.7)) * TimeLineData.length);
+      setActiveItem(index);
+    }
+  }
 
-      //     setActiveItem(index);
-      //   }
-      // }
+  // snap back to beginning of scroll when window is resized
+  // avoids a bug where content is covered up if coming from smaller screen
+  useEffect(() => {
+    const handleResize = () => {
+      scroll(carouselRef.current, 0);
+    }
 
-      // // snap back to beginning of scroll when window is resized
-      // // avoids a bug where content is covered up if coming from smaller screen
-      // useEffect(() => {
-      //   const handleResize = () => {
-      //     scroll(carouselRef.current, 0);
-      //   }
+    window.addEventListener('resize', handleResize);
+  }, []);
 
-      //   window.addEventListener('resize', handleResize);
-      // }, []);
+  return (
+    <Section id="about">
+      <SectionDivider />
+      <br />
 
-      return (
-        <Section id="about">
-          <SectionTitle>
-            About Me
-          </SectionTitle>
+      <SectionTitle>About Me</SectionTitle>
 
-          <SectionText>
-            The purpose of JavaScript Mastery is to help aspiring and establishing developers to elevate their skills. Building meaningful applications and contributing abck to the community
-          </SectionText>
+      <SectionText>
+        The purpose of JavaScript Mastery is to help aspiring and established developers to take their development skills to the next level and build awesome apps.
+      </SectionText>
 
-          <CarouselContainer ref={carouselRef}>
-            <>
-              {/* Loop over the TimeLineData */}
-              {TimeLineData.map((item, index) => (
-                // Show data
-                <CarouselMobileScrollNode
-                  key={index}
-                  final={index === TOTAL_CAROUSEL_COUNT - 1}
-                >
-                  <CarouselItem
-                    index={index}
-                    id={`carousel__item-${index}`}
-                    active={activeItem}
-                    onClick={(e) => handleClick}
+      <CarouselContainer ref={carouselRef} onScroll={handleScroll}>
+        <>
+          {TimeLineData.map((item, index) => (
+            <CarouselMobileScrollNode
+              key={index}
+              final={index === TOTAL_CAROUSEL_COUNT - 1}
+            >
+
+              <CarouselItem
+                index={index}
+                id={`carousel__item-${index}`}
+                active={activeItem}
+                onClick={(e) => handleClick(e, index)}
+              >
+
+                <CarouselItemTitle>
+                  {`${item.year}`}
+
+                  <CarouselItemImg
+                    width="208"
+                    height="6"
+                    viewBox="0 0 208 6"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
                   >
-                    <CarouselItemTitle>
-                      
-                    </CarouselItemTitle>
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M2.5 5.5C3.88071 5.5 5 4.38071 5 3V3.5L208 3.50002V2.50002L5 2.5V3C5 1.61929 3.88071 0.5 2.5 0.5C1.11929 0.5 0 1.61929 0 3C0 4.38071 1.11929 5.5 2.5 5.5Z"
+                      fill="url(#paint0_linear)"
+                      fill-opacity="0.33"
+                    />
+                    <defs>
+                      <linearGradient
+                        id="paint0_linear"
+                        x1="-4.30412e-10"
+                        y1="0.5"
+                        x2="208"
+                        y2="0.500295"
+                        gradientUnits="userSpaceOnUse">
+                        <stop stop-color="white" />
+                        <stop
+                          offset="0.79478"
+                          stop-color="white"
+                          stop-opacity="0"
+                        />
+                      </linearGradient>
+                    </defs>
+                  </CarouselItemImg>
+                </CarouselItemTitle>
+                <CarouselItemText>{item.text}</CarouselItemText>
+              </CarouselItem>
+            </CarouselMobileScrollNode>
+          ))}
+        </>
+      </CarouselContainer>
+      
+      <CarouselButtons>
+        {TimeLineData.map((item, index) => {
+          return (
+            <CarouselButton
+              key={index}
+              index={index}
+              active={activeItem}
+              onClick={(e) => handleClick(e, index)}
+              type="button"
+            >
 
-                  </CarouselItem>
-                </CarouselMobileScrollNode>
-              ))}
-            </>
-          </CarouselContainer>
-        </Section>
-      );
-    };
+              <CarouselButtonDot active={activeItem} />
+            </CarouselButton>
+          );
+        })}
+      </CarouselButtons>
+    </Section>
+  );
+};
 
-    export default Timeline;
+export default Timeline;
