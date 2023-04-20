66dcd33371f08ee75003c44ba167954acc6069d6
Matthew Ng <nglingshan30@gmail.com>
2023-04-20 01:45:39 -0600

    Create Data.js

---
+++ src/Assets/Data.js
@@ -0,0 +1,46 @@
+export const projects = [
+  {
+    title: 'BioJewelery E-Commerce',
+    description: "Using MERN stack, my team created a fully functional e-commerce website for a startup company that is now based in Calgary. ",
+    image: '/images/4.png',
+    tags: ['Mongo', 'Express', 'React', 'Node'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 0,
+  },
+  {
+    title: 'Modern UI / UX Restaurant',
+    description: "Using MERN stack, my team created a fully functional e-commerce website for a startup company that is now based in Calgary. ",
+    image: '/images/4.png',
+    tags: ['React', 'JavaScript'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 1,
+  },
+  {
+    title: 'Blog',
+    description: "Sharing is caring. This blogs will be posted here for people who want to dive into various tech topics",
+    image: '/images/3.png',
+    tags: ['HTML','CSS', 'Markdown'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 2,
+  },
+  {
+    title: 'Unichat',
+    description: "Sharing is caring. This blogs will be posted here for people who want to dive into various tech topics",
+    image: '/images/4.png',
+    tags: ['React', 'ChatEngine', 'Firebase'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 3,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2020, text: 'Started my journey', },
+  { year: 2021, text: 'Inrolled in the SAIT IT SD diploma course', },
+  { year: 2022, text: 'Start creating an ecommerce for startup BioJewelery', },
+  { year: 2023, text: 'Complete and delevered our website', },
+  // { year: 2024, text: 'Worked as a freelance developer', },
+];
