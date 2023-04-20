c58c2b74c3c5d4bf391c66d334491014552f83f0
Matthew Ng <nglingshan30@gmail.com>
2023-04-20 01:47:11 -0600

    Modified documents

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Acomplishments from '../components/Acomplishments/Acomplishments';
 import BgAnimation from '../components/BackgrooundAnimation/BackgroundAnimation';
-import Hero from '../components/Hero/Hero';
+import Header from '../components/Header/Header';
 import Projects from '../components/Projects/Projects';
 import Technologies from '../components/Technologies/Technologies';
 import Timeline from '../components/TimeLine/TimeLine';
@@ -11,7 +11,7 @@ const Home = () => {
   return (
     <Layout>
       <Section grid>
-        <Hero />
+        <Header />
         <BgAnimation />
       </Section>
       <Projects />
