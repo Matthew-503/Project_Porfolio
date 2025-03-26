6eae164168e53d30a47c394bf0eb2dad88b3e6fe
Matthew Ng <98842287+Matthew-503@users.noreply.github.com>
2025-03-26 00:07:23 -0600

    Update Data.js

--- src/Assets/Data.js
+++ src/Assets/Data.js
@@ -1,46 +1,46 @@
-// export const projects = [
-//   {
-//     title: 'BioJewelery E-Commerce',
-//     description: "Using MERN stack, my team created a fully functional e-commerce website for a startup company that is now based in Calgary",
-//     image: '/images/4.png',
-//     tags: ['Mongo', 'Express', 'React', 'Node'],
-//     source: 'https://biojewelryapp.onrender.com/',
-//     code: 'https://github.com/Matthew-503/BioJewellery-Capstone',
-//     id: 0,
-//   },
-//   {
-//     title: 'Modern UI / UX Restaurant',
-//     description: "A cutting-edge restaurant webpage using React and CSS unite for a captivating modern design",
-//     image: '/images/4.png',
-//     tags: ['React', 'JavaScript'],
-//     source: 'https://restaurant-503.vercel.app/',
-//     code: 'https://github.com/Matthew-503/Project_Restaurant-Application',
-//     id: 1,
-//   },
-//   {
-//     title: 'Blog',
-//     description: "Sharing is caring. This blogs will be posted here for people who want to dive into various tech topics",
-//     image: '/images/3.png',
-//     tags: ['HTML', 'CSS', 'Markdown'],
-//     source: 'https://github.com/Matthew-503/Matthew-503.github.io',
-//     code: 'https://matthew-503.github.io/',
-//     id: 2,
-//   },
-//   {
-//     title: 'BrieflyAI',
-//     description: "Let AI merge into your daily life by summarizing your reading, giving you the extra boost you need",
-//     image: '/images/5.png',
-//     tags: ['React', 'RapidAPI', 'Tailwind'],
-//     source: 'https://breiflyai.vercel.app/',
-//     code: 'https://github.com/Matthew-503/project_ai-summarizer',
-//     id: 3,
-//   },
-// ];
+export const projects = [
+  {
+    title: 'BioJewelery E-Commerce',
+    description: "Using MERN stack, my team created a fully functional e-commerce website for a startup company that is now based in Calgary",
+    image: '/images/4.png',
+    tags: ['Mongo', 'Express', 'React', 'Node'],
+    source: 'https://biojewelryapp.onrender.com/',
+    code: 'https://github.com/Matthew-503/BioJewellery-Capstone',
+    id: 0,
+  },
+  {
+    title: 'Modern UI / UX Restaurant',
+    description: "A cutting-edge restaurant webpage using React and CSS unite for a captivating modern design",
+    image: '/images/4.png',
+    tags: ['React', 'JavaScript'],
+    source: 'https://restaurant-503.vercel.app/',
+    code: 'https://github.com/Matthew-503/Project_Restaurant-Application',
+    id: 1,
+  },
+  {
+    title: 'Blog',
+    description: "Sharing is caring. This blogs will be posted here for people who want to dive into various tech topics",
+    image: '/images/3.png',
+    tags: ['HTML', 'CSS', 'Markdown'],
+    source: 'https://github.com/Matthew-503/Matthew-503.github.io',
+    code: 'https://matthew-503.github.io/',
+    id: 2,
+  },
+  {
+    title: 'BrieflyAI',
+    description: "Let AI merge into your daily life by summarizing your reading, giving you the extra boost you need",
+    image: '/images/5.png',
+    tags: ['React', 'RapidAPI', 'Tailwind'],
+    source: 'https://breiflyai.vercel.app/',
+    code: 'https://github.com/Matthew-503/project_ai-summarizer',
+    id: 3,
+  },
+];
 
-// export const TimeLineData = [
-//   { year: 2020, text: 'Started my journey', },
-//   { year: 2021, text: 'Inrolled in the SAIT IT SD diploma course', },
-//   { year: 2022, text: 'Start creating an ecommerce for startup BioJewelery', },
-//   { year: 2023, text: 'Complete and delevered our website', },
-//   // { year: 2024, text: 'Worked as a freelance developer', },
-// ];
+export const TimeLineData = [
+  { year: 2020, text: 'Started my journey', },
+  { year: 2021, text: 'Inrolled in the SAIT IT SD diploma course', },
+  { year: 2022, text: 'Start creating an ecommerce for startup BioJewelery', },
+  { year: 2023, text: 'Complete and delevered our website', },
+  // { year: 2024, text: 'Worked as a freelance developer', },
+];
